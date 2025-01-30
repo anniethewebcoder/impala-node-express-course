1f0d59f5db381f373dace7a0139c6b9c38bcb975
Annie <annie.the.web.coder@gmail.com>
2025-01-30 16:56:49 -0500

    mongoose

---
+++ 03-task-manager/starter/db/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const connectionString = "mongodb+srv://annie:rT1uMq7Byg1t3fGY@nodeexpressprojects.bv3xntw.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProjects"
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB'))
+.catch((err) => console.log(err))
