d8f6e5b6cf9b012a9f182dfef85f3f8b6962d965
Annie <annie.the.web.coder@gmail.com>
2025-02-01 22:20:58 -0500

    errors, middleware

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -5,6 +5,9 @@ const connectDB = require("./db/connect");
 
 require("dotenv").config();
 
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
 app.use(express.static('./public'))
 app.use(express.json());
 
@@ -14,7 +17,10 @@ app.get("/hello", (req, res) => {
 
 app.use("/api/v1/tasks", tasks);
 
-const port = 3000;
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
