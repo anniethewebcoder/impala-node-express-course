9d869f725ce805996ce1ce02d44ea1c1ea5d3c50
anniethewebcoder <anniethewebcoder@gmail.com>
2025-02-05 01:17:16 -0500

    schema

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,28 +1,35 @@
-require('dotenv').config()
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const notFoundMiddleware = require('./middleware/not-found')
-const errorMiddleware = require('./middleware/error-handler')
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
-app.use(express.json())
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
 
-app.get('/', (req, res) => {
-    res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>')
-})
+app.use(express.json());
 
-app.use(notFoundMiddleware)
-app.use(errorMiddleware)
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>');
+});
 
-const port = process.env.PORT || 3000
+app.use("/api/v1/products", productsRouter);
 
-const start = async() => {
-    try {
-        app.listen(port, console.log(`Server is listening port ${port}`))
-    } catch (error) {
-        console.log(error)
-    }
-}
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
-start()
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
