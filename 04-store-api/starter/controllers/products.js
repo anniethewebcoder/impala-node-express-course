0357243013905a3f6c766107d84874ebb3729948
anniethewebcoder <anniethewebcoder@gmail.com>
2025-02-05 11:09:05 -0500

    query searches

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,20 @@
+const { query } = require("express");
 const Product = require("./../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   //   throw new Error("testing async errors");
+
+  const search = "a";
+
   const products = await Product.find({
-    featured: true,
-  });
+    name: { $regex: search, $options: "i" },
+    price: { $gt: 30 },
+  })
+    .sort("-name price")
+    .select("name price")
+    .limit(4)
+    .skip(1);
+
   res.status(200).json({
     products,
     nbHits: products.length,
@@ -12,8 +22,46 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+
   res.status(200).json({
-    msg: "product route",
+    products,
+    nbHits: products.length,
   });
 };
 
